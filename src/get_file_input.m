17f59347522f6728c3768fec51845438d5c34257
Jacob Killelea <Jkillelea@protonmail.ch>
2016-08-29 13:58:50 -0600

    now ignores comments

--- src/get_file_input.m
+++ src/get_file_input.m
@@ -2,10 +2,15 @@ function [] = get_file_input(filepath)
   % handles file reading
   fileID = fopen(filepath);
 
-  line = 0;
+  line = fgets(fileID);
   while line > -1 % fgets returns -1 for End Of File (EOF)
+    if sum(line(1) == '#')
+      % nothing
+    else
+      disp(line);
+    end
+
     line = fgets(fileID);
-    disp(line);
   end
 
 
