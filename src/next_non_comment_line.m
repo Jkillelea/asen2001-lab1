d6c7b220a5b68fd4bbf2800d0a3923dafe630bec
Jacob Killelea <Jkillelea@protonmail.ch>
2016-09-01 12:39:10 -0600

    commenting

--- src/next_non_comment_line.m
+++ src/next_non_comment_line.m
@@ -1,3 +1,5 @@
+% Copyright Jacob Killelea 2016
+% GPL V3
 % returns the next line that doesn't begin with a #
 % if this function is called repeatedly, it will return a new line each time, until EOF
 function line = next_non_comment_line(fileID)
