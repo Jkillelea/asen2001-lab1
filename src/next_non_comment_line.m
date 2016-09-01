6c9b7e2dea121f4290701a7d172279c14e928e0d
Jacob Killelea <Jkillelea@protonmail.ch>
2016-09-01 12:38:42 -0600

    commenting

--- src/next_non_comment_line.m
+++ src/next_non_comment_line.m
@@ -1,3 +1,5 @@
+% returns the next line that doesn't begin with a #
+% if this function is called repeatedly, it will return a new line each time, until EOF
 function line = next_non_comment_line(fileID)
   line = fgets(fileID);
   while (line(1) == '#')
