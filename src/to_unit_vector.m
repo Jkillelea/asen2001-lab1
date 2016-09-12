f995cf29ced0410599bb4a53d9441a6f54c9abc0
Jacob Killelea <Jkillelea@protonmail.ch>
2016-09-12 13:58:12 -0600

    marked deprecated

--- src/to_unit_vector.m
+++ src/to_unit_vector.m
@@ -1,5 +1,9 @@
+% DEPRECATED
+% DO NOT USE
+
 function [x, y, z] = to_unit_vector(dx, dy, dz)
-  vector_magnitude = magnitude(dx, dy, dz);
+  disp('WARNING:: to_unit_vector is a deprecated function');
+  vector_magnitude = magnitude([dx, dy, dz]);
 
   x = dx/vector_magnitude;
   y = dy/vector_magnitude;
