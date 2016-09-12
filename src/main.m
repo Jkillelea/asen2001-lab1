94e10a87910d7db10491d236136a43171100236e
Jacob Killelea <Jkillelea@protonmail.ch>
2016-09-12 13:42:07 -0600

    misc

--- src/main.m
+++ src/main.m
@@ -3,3 +3,5 @@
 
 addpath(genpath('.')); % add all subdirectories (./src, ./examples, ect) to path
 clear all; clc;
+
+[num_forces, force_application_coords, force_vector_coords, num_moments, moment_application_coords, moment_vector_coords, NUM_SUPPORTS, support_coords, support_reaction_data] = get_file_input('data/Lab1_Input.txt')
