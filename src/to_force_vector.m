924a8714a7d5f02ef83886522648714c55b847f0
Jacob Killelea <Jkillelea@protonmail.ch>
2016-09-12 22:56:26 -0600

    function for computing x,y,z components of vector based on magnitude and direction

---
+++ src/to_force_vector.m
@@ -0,0 +1,13 @@
+function vector = to_force_vector(force)
+    % Returns a vector based on the direction and magnitude of the input vector, [f, dx, dy, dz]
+    % [x, y, z] = to_force_vector([f, dx, dy, dz])
+    % the magnitudes of dx, dy, and dz do not matter. Only their porportions relative to each other are important.
+    % The returned vector is of magnitude f.
+    % this function is input-agnostic, and could be called with parameter {f, dx, dy, dz}
+    if isa(force, 'cell') % if force is cell, change to vector
+      force = cell2mat(force);
+    end
+
+    magnitude_force = force(1); % get value inside vector
+    vector = magnitude_force .* to_unit_vector(force(2:4));
+end
