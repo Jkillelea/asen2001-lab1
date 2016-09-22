73b4e9189ce497a24d70dee8b14128955d79fa21
Jacob Killelea <Jkillelea@protonmail.ch>
2016-09-22 10:47:38 -0600

    now writes out changes to outfile_path

--- src/output.m
+++ src/output.m
@@ -1,3 +1,21 @@
-function [] = output(x_matrix, outfile_path)
+function [success] = output(outfile_path, data_cell)
+  % fprintf(fileID,formatSpec,A1,...,An) writes to a file nicely
 
+
+  fileID = fopen(outfile_path, 'w'); % discard previous contents of file
+
+  if(fileID == -1) % coudln't open file
+    fileID = 1;    % redirect to STDOUT
+  end
+
+  num_lines = size(data_cell, 1);
+
+  for i = 1:num_lines
+    force_or_moment = data_cell{i, 1};
+    magnitude       = data_cell{i, 2};
+    fprintf(fileID, '%s | %d\n', force_or_moment, magnitude);
+  end
+
+  fclose(fileID);
+  success = true;
 end
