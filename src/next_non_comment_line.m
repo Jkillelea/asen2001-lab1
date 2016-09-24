50254d2dc7abcf90081cad01f94d4196fe2e29f6
Jacob Killelea <Jkillelea@protonmail.ch>
2016-09-24 12:41:26 -0600

    updated copyright header and comments

--- src/next_non_comment_line.m
+++ src/next_non_comment_line.m
@@ -1,8 +1,7 @@
-% Copyright Jacob Killelea 2016
-% MIT License
-% returns the next line that doesn't begin with a #
-% if this function is called repeatedly, it will return a new line each time, until EOF
+% Copyright Ben Hageneu and Jacob Killelea
 function line = next_non_comment_line(fileID)
+  % returns the next line that doesn't begin with a #
+  % if this function is called repeatedly, it will return a new line each time, until EOF
   line = fgets(fileID);
   while (line(1) == '#')
     line = fgets(fileID);
