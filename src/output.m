49ccde5b56ec0665c48d7d28527f632102140df4
Jacob Killelea <Jkillelea@protonmail.ch>
2016-09-24 12:42:14 -0600

    output file now includes the input file in output, banner and nicely formatted data

--- src/output.m
+++ src/output.m
@@ -1,20 +1,32 @@
-function [] = output(outfile_path, data_cell)
+% Copyright Ben Hageneu and Jacob Killelea
+function [] = output(outfile_path, infile_path, data_cell)
   % fprintf(fileID,formatSpec,A1,...,An) writes to a file nicely
 
+  infile  = fopen(infile_path,  'r');
+  outfile = fopen(outfile_path, 'w'); % discard previous contents of file
 
-  fileID = fopen(outfile_path, 'w'); % discard previous contents of file
-
-  if(fileID == -1) % coudln't open file
-    fileID = 1;    % redirect to STDOUT
+  if(outfile == -1) % coudln't open file
+    outfile = 1;    % redirect to STDOUT
   end
 
-  num_lines = size(data_cell, 1);
+  fprintf(outfile, '# Output file for problem data in %s\n# Software and associated data is Copyright Ben Hagenau and Jacob Killelea 2016\n#####################\n', infile_path );
 
+  line = fgets(infile);
+  while line ~= -1
+    fprintf(outfile, line);
+    line = fgets(infile);
+  end
+
+  fprintf(outfile, '# type (F/M) and magnitude of reaction forces\n');
+  fprintf(outfile, '# type  magnitude\n');
+
+  num_lines = size(data_cell, 1);
   for i = 1:num_lines
     force_or_moment = data_cell{i, 1};
     magnitude       = data_cell{i, 2};
-    fprintf(fileID, '%s %f\n', force_or_moment, magnitude);
+    fprintf(outfile, '%s       %f\n', force_or_moment, magnitude);
   end
 
-  fclose(fileID);
+  fclose(infile);
+  fclose(outfile);
 end
